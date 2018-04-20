5d7592b1e293848b728e667b601bafe69b11f565
Morris <morris.lwt@gmail.com>
2018-04-20 13:50:42 +0800

    Initial Commit

---
+++ SlideOutMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideOutMenu
+//
+//  Created by Morris on 2018/4/20.
+//  Copyright © 2018年 Morris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
