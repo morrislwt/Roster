5b0445e386a2515b8f37a8fbfb19c5fda479923c
Morris Lu <morris.lwt@gmail.com>
2018-05-30 15:12:59 +0800

    before set login function with firebase

--- Roster/AppDelegate.swift
+++ Roster/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -17,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         print(Realm.Configuration.defaultConfiguration.fileURL)
+        FirebaseApp.configure()
         return true
     }
 
---
+++ Roster/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  Roster
+//
+//  Created by Morris on 2018/5/30.
+//  Copyright © 2018年 Morris. All rights reserved.
+//
+
+import UIKit
+
+
+class LoginViewController:UIViewController{
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBAction func startButton(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
